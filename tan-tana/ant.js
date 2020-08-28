4f0e2887ff1d4c3b5d2587aec06f60721cdba71b
shruti3415 <66659405+shruti3415@users.noreply.github.com>
2020-08-29 01:38:42 +0530

    Add files via upload

---
+++ tan-tana/ant.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("dialog");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunc() {
+  var x = document.getElementById("video");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
